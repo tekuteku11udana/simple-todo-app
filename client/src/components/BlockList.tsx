9bbe054aee82726295653b1b9209977556ab6aa5
tekuteku11udana <tekuteku11udana@gmail.com>
2022-01-21 09:38:55 +0900

    Test access to backend API with fetch().

--- client/src/components/BlockList.tsx
+++ client/src/components/BlockList.tsx
@@ -1,3 +1,5 @@
+import axios from "axios"
+import { useEffect } from "react"
 import TextBlock from "./TextBlock"
 
 const BlockList = () => {
@@ -6,6 +8,14 @@ const BlockList = () => {
         {key:2, text: "cd"},
         {key:3, text: "ef"}
     ]
+    
+    useEffect(() => {
+        fetch("/blocks/")
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        
+        
+    },[])
 
    return (
        <ul>
