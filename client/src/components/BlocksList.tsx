5005c5db58beb343b0f506df3514b1bc3eb1c560
tekuteku11udana <tekuteku11udana@gmail.com>
2022-01-27 00:13:00 +0900

    introduce react-beautiful-dnd

--- client/src/components/BlocksList.tsx
+++ client/src/components/BlocksList.tsx
@@ -1,6 +1,7 @@
-
+/** @jsxImportSource @emotion/react */
 import { css, jsx } from '@emotion/react'
 import { createContext, createRef, useContext, useEffect, useRef, useState } from "react"
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
 import { BlocksContext } from "../providers/BlocksProvider";
 import { FocusedIndexContext } from "../providers/FocusedIndexProvider";
 import { Block } from "../type/type";
@@ -11,21 +12,53 @@ import TextBlock from "./TextBlock"
 
 
 const BlocksList = () => {
-   const {blocks} = useContext(BlocksContext)
-   const {focusedIndex} = useContext(FocusedIndexContext)
+    const {blocks, setBlocks} = useContext(BlocksContext)
+    const {focusedIndex} = useContext(FocusedIndexContext)
 
-   return (     
-        <ul >
-            {blocks.map((block, index) => 
-                <li key={block.id}>
-                    <TextBlock 
-                        id={block.id} 
-                        index={index} 
-                        isFocused={focusedIndex === index}
-                    />
-                </li>)
-            }
-        </ul>
+    const handleOnDrugEnd = (result: DropResult) => {
+        const newBlocks = Array.from(blocks)
+        const [reorderedBlock] = newBlocks.splice(result.source.index, 1)
+        if (!result.destination) return
+        newBlocks.splice(result.destination.index, 0, reorderedBlock)
+
+        setBlocks(blocks)
+    }
+
+    return (     
+        <DragDropContext 
+                onDragEnd={handleOnDrugEnd}
+            >
+        
+            
+            <Droppable droppableId='1'>
+                {provided => (
+                    <ul {...provided.droppableProps} ref={provided.innerRef}>
+                        {blocks.map((block, index) => 
+                            <Draggable key={block.id} draggableId={block.id} index={index}>
+                                {provided => (
+                                    <li key={block.id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} 
+                                        css={css`
+                                            margin: 8px;
+                                            border: 1px solid lightgrey;
+                                            border-radius: 2px
+                                        `}>
+                                        <TextBlock 
+                                            id={block.id} 
+                                            index={index} 
+                                            isFocused={focusedIndex === index}
+                                        />
+                                    </li>
+                                )}
+                                
+                            </Draggable>
+                            )
+                        }
+                        {provided.placeholder}
+                    </ul>
+                )}
+            </Droppable>
+       </DragDropContext>
+        
        
        
    ) 
